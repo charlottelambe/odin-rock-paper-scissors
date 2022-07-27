3e429217efb74705b8343dcf5790eb65cdbe8ac7
charlottelambe <92045197+charlottelambe@users.noreply.github.com>
2022-07-27 10:03:13 +0100

    Created a playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,32 @@
 function getComputerChoice() {
-	var items = ["Rock", "Paper", "Scissors"];
+	var items = ["rock", "paper", "scissors"];
     let random_item = items[Math.floor(Math.random()*3)];
     return random_item;
  }
-console.log(getComputerChoice());
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+	return "You win! Rock beats Scissors";
+}
+else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+	return "You win! Scissors beats Paper";
+}
+else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+	return "You win! Paper beats Rock";
+}
+else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
+	return "You lose! Rock beats Scissors";
+}
+else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
+	return "You lose! Scissors beats Paper";
+}
+else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
+	return "You lose! Paper beats Rock";
+}
+else if (playerSelection.toLowerCase() === computerSelection) {
+	return "It's a draw!";
+}
+}
+const playerSelection = "rock";
+console.log(playRound(playerSelection, computerSelection));
